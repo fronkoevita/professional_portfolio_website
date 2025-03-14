51d8dbc4b3fd71b54d45cf190b9f6c76f2debfe3
fronkoevita <fronkoeva@gmail.com>
2025-03-14 21:58:18 +0100

    Added .gitignore and ignored .env

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+from flask_bootstrap5 import Bootstrap
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
+# ------------ DATABASE MODELS -------------
+class ContactMessage(db.Model):
+    __tablename__ = "contact_messages"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+# ------------ WTForms -------------
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send Message")
+
+# ------------ ROUTES -------------
+@app.route("/", methods=["GET", "POST"])
+def home():
+    form = ContactForm()
+    if form.validate_on_submit():
+        new_message = ContactMessage(
+            name=form.name.data,
+            email=form.email.data,
+            message=form.message.data
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("index.html", form=form)
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        new_message = ContactMessage(
+            name=form.name.data,
+            email=form.email.data,
+            message=form.message.data
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return '''<script>alert("Your message has been successfully sent!"); window.location.href="/";</script>'''
+
+    return render_template("contact.html", form=form)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
